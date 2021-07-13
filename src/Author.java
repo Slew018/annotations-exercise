ddf1953919191287ff2cc9b4081f573cf1883a54
Sean Lewis <sean.lewis018@gmail.com>
2021-07-13 14:32:36 -0500

    more bug fixes

--- src/Author.java
+++ src/Author.java
@@ -1,10 +1,7 @@
-/**
- * This source file is subject to the license that is bundled with this package in the file LICENSE.
- */
-
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("ALL")
 public class Author extends Person {
     private List books;
 
